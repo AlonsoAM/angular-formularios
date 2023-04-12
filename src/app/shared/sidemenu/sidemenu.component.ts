6fa3d248902bb3cc18b50d38f96b7890252bb1cd
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-04-12 12:06:25 -0500

    Agregando componentes y configurando rutas

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { MenuItem } from '../interfaces/shared.interface';
 
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SidemenuComponent {
-
+  templateMenu: MenuItem[] = [
+    { texto: 'Básicos', ruta: './template/basicos' },
+    { texto: 'Dinámicos', ruta: './template/dinamicos' },
+    { texto: 'Switches', ruta: './template/switches' },
+  ];
+  reactiveMenu: MenuItem[] = [
+    { texto: 'Básicos', ruta: './reactive/basicos' },
+    { texto: 'Dinámicos', ruta: './reactive/dinamicos' },
+    { texto: 'Switches', ruta: './reactive/switches' },
+  ];
 }
